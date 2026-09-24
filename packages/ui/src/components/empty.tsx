4b2f277f9f1696a0474ab434e6758ccffae2e12a
Sadman Sakib <68962712+sadmann7@users.noreply.github.com>
2026-09-25 00:21:24 +0600

    chore: migrate to cn (#81)

--- packages/ui/src/components/empty.tsx
+++ packages/ui/src/components/empty.tsx
@@ -1,7 +1,7 @@
 import type * as React from "react";
 
-import { cn } from "@pptx/ui/lib/utils";
 import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "cn";
 
 function Empty({ className, ...props }: React.ComponentProps<"div">) {
   return (
